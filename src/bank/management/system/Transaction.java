472a22034ccde03d53720af598f850c93991cc46
Amartya Ghosh <amartyaghosh48@gmail.com>
2023-11-15 00:05:51 +0530

    Upload all the files

--- src/bank/management/system/Transaction.java
+++ src/bank/management/system/Transaction.java
@@ -81,6 +81,9 @@ public class Transaction extends JFrame implements ActionListener {
         } else if (ae.getSource() == withdrawl) {
             setVisible(false);
             new Withdrawl(pinnumber).setVisible(true);
+        } else if (ae.getSource() == fastcash) {
+            setVisible(false);
+            new FastCash(pinnumber).setVisible(true);
         }
     }
     
